9abcd818ff3f697b547af9ff4f2af5c4cafc1884
James Dawson <jamesdawsonWD@gmail.com>
2019-05-28 23:05:30 +0100

    feature: retrieve all image data successful

--- src/app/containers/image-view-container/image-container.component.ts
+++ src/app/containers/image-view-container/image-container.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HTTPservice } from '@core/services/http.service';
-
+import { Image } from '@lib/models';
+import { getThumbNail } from '@lib/helpers';
+import { environment } from '@environments/environment';
 @Component({
   selector: 'app-image-container',
   templateUrl: './image-container.component.html',
 })
 export class ImageContainerComponent implements OnInit {
-
+  public serverUrl = this.http.serverUrl;
   public allImages$ = this.http.getAllImages();
+  public getThumbNail = getThumbNail;
   constructor(private http: HTTPservice) { }
 
   ngOnInit() {
   }
+
 
 }
